93bec5c11c73d6c0de3ebe96ca9d4f3bec9f9edf
coder4509 <60377831+coder4509@users.noreply.github.com>
2023-08-21 17:55:29 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -6,7 +6,7 @@ const fs = require('fs');
 app.use(express.static('./public'));
 
 app.get('/api', (req, res) => {
-  fs.writeFileSync('text.json', JSON.strigify({t: 1, t2: 3}), 'utf8');
+  fs.writeFileSync('text.json', JSON.stringify({t: 1, t2: 3}), 'utf8');
   const readData = fs.readFileSync('text.json', 'utf8');
   res.send(readData);
   //res.sendFile(path.resolve('./pages/index.html'));
