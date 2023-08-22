c3423ee4084d11375b161634a3e49836e1ddf9fe
coder4509 <60377831+coder4509@users.noreply.github.com>
2023-08-22 21:04:59 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -15,19 +15,20 @@ app.get('/', (req, res) => {
 
 app.post('/', upload.single('uploaded_file'), (req, res) => {
 const {width, height} = req.body;
-const {buffer, mimetype} = req.file;
+const {buffer, mimetype, originalname} = req.file;
 sharp(buffer).resize(200, 230)
   .jpeg({ mozjpeg: true })
   .toBuffer()
   .then( data => {
     const fileData = Buffer.from(data).toString('base64');
+    const uid = Date.now();
     res.send( `<div>
     <div>
       <a href='/'> Back </a>
     </div>
     <hr/>
     <br/>
-    <a href='data:${mimetype};base64,${fileData}' download>
+    <a href='data:${mimetype};base64,${fileData}' download='${originalname}_${uid}'>
        <img src= 'data:${mimetype};base64,${fileData}' width='200' height='200' />
     </a>
     </div>`);
