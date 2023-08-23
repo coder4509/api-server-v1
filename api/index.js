7188d5fbd29a21281ecef5972456dd2022efcc95
coder4509 <60377831+coder4509@users.noreply.github.com>
2023-08-23 16:06:08 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -14,8 +14,9 @@ app.get('/', (req, res) => {
   res.sendFile(path.resolve('./pages/index.html'));
 });
 
-app.post('/', upload.single('uploaded_file'), (req, res) => {
+app.post('/compress', upload.single('uploaded_file'), (req, res) => {
 const {width, height} = req.body;
+  console.log(req.body, req.file);
 const {buffer, mimetype, originalname} = req.file;
 sharp(buffer).resize(200, 230)
   .jpeg({ mozjpeg: true })
@@ -23,18 +24,12 @@ sharp(buffer).resize(200, 230)
   .then( data => {
     const fileData = Buffer.from(data).toString('base64');
     const uid = Date.now();
-    res.send( `<div>
-    <div>
-      <a href='/'> Back </a>
-    </div>
-    <hr/>
-    <br/>
-    <a href='data:${mimetype};base64,${fileData}' download>
-       <img src= 'data:${mimetype};base64,${fileData}' width='200' height='200' />
-    </a>
-    </div>`);
+    res.send({
+      status: 200,
+      data: `data:${mimetype};base64,${fileData}`
+    });
   })
-  .catch( err => { console.log(err); res.send('error...') });
+  .catch( err => { console.log(err); res.send({status: 500, data: null}) });
 });
 
 app.get('/health-check', (req, res) => {
