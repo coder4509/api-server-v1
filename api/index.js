ba7fca2d09c374799b54dd5223ebf476b13b6ec9
coder4509 <60377831+coder4509@users.noreply.github.com>
2023-08-24 23:10:06 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,45 +1,50 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const path = require('path');
-const fs = require('fs');
-const sharp = require('sharp');
-const multer  = require('multer');
-const axios = require('axios');
+const path = require("path");
+const fs = require("fs");
+const sharp = require("sharp");
+const multer = require("multer");
+const axios = require("axios");
 
 const upload = multer();
 
-app.use(express.static('./public'));
+app.use(express.static("./public"));
 
-app.get('/', (req, res) => {
-  res.sendFile(path.resolve('./pages/index.html'));
+app.get("/", (req, res) => {
+  res.sendFile(path.resolve("./pages/index.html"));
 });
 
-app.post('/compress', upload.single('uploaded_file'), (req, res) => {
-const {width, height} = req.body;
+app.post("/compress", upload.single("uploaded_file"), (req, res) => {
+  const { width, height } = req.body;
   console.log(req.body, req.file);
-const {buffer, mimetype, originalname} = req.file;
-sharp(buffer).resize(200, 230)
-  .jpeg({ mozjpeg: true })
-  .toBuffer()
-  .then( data => {
-    const fileData = Buffer.from(data).toString('base64');
-    const uid = Date.now();
-    res.send({
-      status: 200,
-      data: `data:${mimetype};base64,${fileData}`
+  const { buffer, mimetype, originalname } = req.file;
+  sharp(buffer)
+    .sharpen()
+    .resize(200, 230)
+    .jpeg({ mozjpeg: true })
+    .toBuffer()
+    .then((data) => {
+      const fileData = Buffer.from(data).toString("base64");
+      const uid = Date.now();
+      res.send({
+        status: 200,
+        data: `data:${mimetype};base64,${fileData}`,
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.send({ status: 500, data: null });
     });
-  })
-  .catch( err => { console.log(err); res.send({status: 500, data: null}) });
 });
 
-app.get('/health-check', (req, res) => {
-  console.log('Server health ok....');
-  res.send('Server health ok....');
+app.get("/health-check", (req, res) => {
+  console.log("Server health ok....");
+  res.send("Server health ok....");
 });
 
-app.listen(8081, ()=> {
+app.listen(8081, () => {
   console.log("Server Running on :: ==>", 8081);
   setInterval(() => {
-    axios.get('https://api-server-4oak.onrender.com/health-check');
+    axios.get("https://api-server-4oak.onrender.com/health-check");
   }, 10000);
 });
